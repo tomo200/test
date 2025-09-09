019dea602ea3cc58414633a230c0bfb8ecb40181
tomo200 <2301305@s.asojuku.ac.jp>
2025-09-09 10:23:45 +0900

    paiza01アップロード

---
+++ puzzle/paiza01.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 --*
+def cal_coinn(amout):
+    amout=int(amout)
+    coin=[500,100,50,10,5,1]
+    result={} #辞書型
+    sums=0
+
+    for c in coin:
+        result[c],amout=divmod(amout,c)
+        sums+=result[c]
+        
+    
+    return sums
+
+change=input()
+ret=cal_coinn(change)
+print(f"{ret}がおつりの数")
+
+
+    
+
+
